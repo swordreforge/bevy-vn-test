112249dc2613350ba2a83d63f31dbd42704671cd
swordreforge <zhujian_20060818@qq.com>
2026-05-25 12:22:34 +0800

    Implement two-stage splash: logo00.png then logo01.png (auto-advance or click)

--- src/plugins/splash.rs
+++ src/plugins/splash.rs
@@ -8,19 +8,30 @@ pub struct SplashPlugin;
 struct SplashRoot;
 
 #[derive(Resource)]
-struct SplashTimer(Timer);
+struct SplashState {
+    phase: SplashPhase,
+    timer: Timer,
+}
+
+enum SplashPhase {
+    Logo0,
+    Logo1,
+}
 
 impl Plugin for SplashPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(AppState::Splash), setup_splash)
-            .add_systems(Update, splash_tick.run_if(in_state(AppState::Splash)))
+            .add_systems(Update, splash_update.run_if(in_state(AppState::Splash)))
             .add_systems(OnExit(AppState::Splash), cleanup_splash);
     }
 }
 
 fn setup_splash(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.init_resource::<SplashTimer>();
+    commands.insert_resource(SplashState {
+        phase: SplashPhase::Logo0,
+        timer: Timer::from_seconds(3.0, TimerMode::Once),
+    });
 
     commands.spawn((
         SplashRoot,
@@ -28,40 +39,54 @@ fn setup_splash(mut commands: Commands, asset_server: Res<AssetServer>) {
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
             position_type: PositionType::Absolute,
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
             ..default()
         },
-        BackgroundColor(Color::BLACK),
-    )).with_child((
-        ImageNode::new(asset_server.load("images/splash.png")),
-        Node {
-            width: Val::Px(300.0),
-            height: Val::Auto,
-            ..default()
-        },
+        ImageNode::new(asset_server.load("images/logo00.png")),
     ));
 }
 
-fn splash_tick(
+fn splash_update(
     time: Res<Time>,
-    mut timer: ResMut<SplashTimer>,
+    mut splash_state: ResMut<SplashState>,
     mut screen_transition: ResMut<ScreenTransition>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    splash_root_query: Query<Entity, With<SplashRoot>>,
+    mouse: Res<ButtonInput<MouseButton>>,
 ) {
-    timer.0.tick(time.delta());
-    if timer.0.just_finished() {
-        screen_transition.pending_state = Some(AppState::Title);
+    splash_state.timer.tick(time.delta());
+    let advance = splash_state.timer.just_finished() || mouse.just_pressed(MouseButton::Left);
+
+    if !advance {
+        return;
+    }
+
+    match splash_state.phase {
+        SplashPhase::Logo0 => {
+            for entity in &splash_root_query {
+                commands.entity(entity).despawn();
+            }
+            commands.spawn((
+                SplashRoot,
+                Node {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    position_type: PositionType::Absolute,
+                    ..default()
+                },
+                ImageNode::new(asset_server.load("images/logo01.png")),
+            ));
+            splash_state.phase = SplashPhase::Logo1;
+            splash_state.timer.reset();
+        }
+        SplashPhase::Logo1 => {
+            screen_transition.pending_state = Some(AppState::Title);
+        }
     }
 }
 
 fn cleanup_splash(mut commands: Commands, query: Query<Entity, With<SplashRoot>>) {
     for entity in &query {
         commands.entity(entity).despawn();
-    }
-}
-
-impl Default for SplashTimer {
-    fn default() -> Self {
-        Self(Timer::from_seconds(3.0, TimerMode::Once))
     }
 }
